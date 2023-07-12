78c860b1393b0b1bf76b2ea5774d9f63478bcf97
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-07-12 18:51:16 +0300

    Reimplement decompilation

--- src/frontend/ClassList.qml
+++ src/frontend/ClassList.qml
@@ -7,15 +7,17 @@ TreeView {
 
     selectionModel: ItemSelectionModel {}
     delegate: TreeViewDelegate {
-        onClicked: {
-            // TODO: Reimplement this
-            // const request = bridge.project.init_decompilation();
-            // for (const index in classes) {
-            //     request.add_class(classes[index]);
-            // }
-            // request.submit();
-            // root.classSelected();
-            // classView.className = classes[0];
+        onDoubleClicked: {
+            const type = model["type"];
+            if (type == "package") return;
+            const classes = model["classes"];
+            const request = bridge.project.init_decompilation();
+            for (const index in classes) {
+                request.add_class(classes[index]);
+            }
+            request.submit();
+            root.classSelected();
+            classView.className = classes[0];
         }
     }
 }
--- src/frontend/package_tree_model.py
+++ src/frontend/package_tree_model.py
@@ -1,12 +1,18 @@
+from enum import IntEnum, auto
 import operator
 
-from PySide6.QtCore import QAbstractItemModel, QModelIndex, QObject, Qt
+from PySide6.QtCore import QAbstractItemModel, QByteArray, QModelIndex, QObject, Qt
+
+
+class PackageTreeRole(IntEnum):
+    Classes = Qt.ItemDataRole.UserRole + 1
+    Type = auto()
 
 
 class PackageTreeModel(QAbstractItemModel):
     def __init__(self, header, parent):
         super().__init__(parent)
-        self.__root = TreeItem(columns=[QObject.tr(text, None) for text in header])
+        self.__root = TreeItem(columns=[QObject.tr(text, None) for text in header], group=[])
 
     def index(self, row, column, parent=QModelIndex()):
         if not self.hasIndex(row, column, parent):
@@ -52,9 +58,9 @@ class PackageTreeModel(QAbstractItemModel):
     def data(self, index, role=Qt.ItemDataRole.DisplayRole.value):
         if not index.isValid():
             return None
-        if role != Qt.ItemDataRole.DisplayRole.value:
-            return None
         item = index.internalPointer()
+        if role != Qt.ItemDataRole.DisplayRole.value:
+            return item.data(role)
         return item.column_data(index.column())
 
     def root(self):
@@ -70,10 +76,17 @@ class PackageTreeModel(QAbstractItemModel):
             return Qt.ItemFlag.NoItemFlags
         return super().flags(index)
 
+    def roleNames(self):
+        roles = dict(super().roleNames())
+        for role in PackageTreeRole:
+            roles[role.value] = QByteArray(role.name.lower().encode("UTF-8"))
+        return roles
+
 
 class TreeItem:
-    def __init__(self, columns, parent=None):
+    def __init__(self, columns, group, parent=None):
         self.__columns = columns
+        self.__group = group
         self.parent = parent
         self.__children = []
 
@@ -101,6 +114,14 @@ class TreeItem:
             return self.__columns[index]
         return None
 
+    def data(self, role):
+        match role:
+            case PackageTreeRole.Classes:
+                return [node.name for node in self.__group]
+            case PackageTreeRole.Type:
+                # TODO: Integrate the type kind thing
+                return "class" if len(self.__group) > 0 else "package"
+
 
 def split_off_package_name(class_name):
     try:
@@ -124,13 +145,13 @@ def create_package_tree_model(class_tree, parent):
         package_name, simple_name = split_off_package_name(head_class)
         if package_name is not None:
             if package_name not in package_items:
-                parent = TreeItem([package_name], parent=root)
+                parent = TreeItem([package_name], group=[], parent=root)
                 root.add_child(parent)
                 package_items[package_name] = parent
             else:
                 parent = package_items[package_name]
         else:
             parent = root
-        item = TreeItem([simple_name], parent)
+        item = TreeItem([simple_name], group, parent)
         parent.add_child(item)
     return model
